e09ab52fabbbf3bdab5cdef73f7f0a79f7fe3fdb
Martin Algañaraz <idcmardelplata@gmail.com>
2019-06-07 19:59:30 -0300

    Example of mock a post call finished

--- endpoints/users/index.spec.js
+++ endpoints/users/index.spec.js
@@ -17,5 +17,30 @@ describe("Endpoints", () => {
         expect(res.send).toHaveBeenCalledWith(1);
       });
     });
+
+    describe("post", () => {
+      it("create a resource", async () => {
+        const axios = {
+          post: jest.fn().mockResolvedValue({ data: 1 })
+        };
+
+        const res = {
+          status: jest.fn().mockReturnThis(),
+          send: jest.fn()
+        };
+        const req = {
+          body: "request body"
+        };
+
+        await handlers({ axios }).post(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.send).toHaveBeenCalled();
+        expect(axios.post).toHaveBeenCalledWith(
+          "https://jsonplaceholder.typicode.com/users",
+          "request body"
+        );
+      });
+    });
   });
 });
