af4545c8b12d7a97576af90452618cca38473a18
Martin Algañaraz <idcmardelplata@gmail.com>
2019-06-10 21:06:16 +0000

    Update index.test.js

--- endpoints/posts/index.test.js
+++ endpoints/posts/index.test.js
@@ -9,7 +9,7 @@ describe("server", () => {
           .post("/")
           .send({ userId: 5 })
           .set("user_id", 1)
-          .send("Content-Type", "application/json");
+          .set("Content-Type", "application/json");
         expect(response.statusCode).toEqual(201);
         expect(response.body.userId).toEqual(5);
         expect(response.body).toHaveProperty("id");
